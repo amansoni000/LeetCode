d9d2ac09fed092479505570a4a03d26035bd7bc8
Aman Swarnkar <80588457+amansoni000@users.noreply.github.com>
2021-12-05 11:06:53 +0530

    Time: 0 ms (100.00%), Space: 37.2 MB (98.03%) - LeetHub

--- sort-colors/sort-colors.java
+++ sort-colors/sort-colors.java
@@ -1,28 +1,33 @@
 class Solution {
-    public void sortColors(int[] nums) {
-        int l,count_0=0,count_1=0,count_2=0;
-        l = nums.length;
-        for(int i:nums){
-            if(i==0){
-                count_0++;
+    public void sortColors(int[] a) {
+            int n=a.length;
+            int a1=0;
+            int b1=2;
+            int temp;
+            int j=0;
+            for(int i=0;i<n;i++){
+                if(a[i]<=a1){
+                    temp=a[i];
+                    a[i]=a[j];
+                    a[j]=temp;
+                    j++;
+                }
             }
-            else if(i==1){
-                count_1++;
+            for(int i=0;i<n;i++){
+                if(a[i]>a1&&a[i]<b1){
+                    temp=a[i];
+                    a[i]=a[j];
+                    a[j]=temp;
+                    j++;
+                }
             }
-            else if(i==2){
-                count_2++;
+            for(int i=0;i<n;i++){
+                if(a[i]>=b1){
+                    temp=a[i];
+                    a[i]=a[j];
+                    a[j]=temp;
+                    j++;
+                }
             }
-            
         }
-        for(int i=0;i<count_0;i++){
-            nums[i]=0;
-        }
-        for(int i=count_0;i<count_1+count_0;i++){
-            nums[i]=1;
-        }
-        for(int i=count_1+count_0;i<l;i++){
-            nums[i]=2;
-        }
-
-    }
 }
