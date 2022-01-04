f13598a2dc5c904e32ae263db2bb648423add3bb
Aman Swarnkar <80588457+amansoni000@users.noreply.github.com>
2022-01-05 00:07:21 +0530

    Time: 1 ms (100.00%), Space: 41.4 MB (82.85%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
@@ -1,34 +1,24 @@
 class Solution {
     public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
-        int satisfied = 0;
-        int cust = customers.length;
-        for(int i=0;i<cust;i++){
-            if(grumpy[i]==0){
-                satisfied+=customers[i];
-            }
+        int sum = 0;
+        
+        int n = customers.length;
+        for(int i = 0; i < n; i++) {
+            sum = sum + customers[i] * (1-grumpy[i]);
+            grumpy[i] = customers[i] * grumpy[i];
         }
-        int start = 0, end = 0;
-        int j = 0, k = 0;
-        int sati_in_grum=0;
-        int min = Integer.MIN_VALUE;
-        for(int i=0; i+minutes-1<cust;i++)
-        {
-            sati_in_grum=0;
-            k=i;
-            j = i+minutes-1;
-            while(k<=j){
-                if(grumpy[k]==1){
-                    sati_in_grum += customers[k];
-                }
-                k++;
-            }
-            if(sati_in_grum>min){
-                
-                min = sati_in_grum;
-                start = i;
-                end = i+minutes-1;
-            }
+        
+        int max = 0;
+        for(int i = 0; i < minutes; i++) {
+            max += grumpy[i];
         }
-        return satisfied+min;
+            
+        int save = max;
+        for(int i = minutes; i < n; i++) {
+            save = save + grumpy[i] - grumpy[i-minutes];
+            max = save > max ? save : max;
+        }
+        
+        return sum + max;
     }
 }
