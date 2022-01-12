dbc41aa379529bc5905f22a6c75e45b5134dfbcb
Aman Swarnkar <80588457+amansoni000@users.noreply.github.com>
2022-01-12 16:27:06 +0530

    Time: 4 ms (56.94%), Space: 39.1 MB (18.16%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.java
+++ 264-ugly-number-ii/264-ugly-number-ii.java
@@ -5,13 +5,12 @@ class Solution {
         int multi2=2, multi3=3, multi5=5;
         int min = Integer.MAX_VALUE;
         dp[0] = 1;
-        for(int i=1; i<n; i++ ){
+        for(int i=1;i<n;i++){
             min = Math.min(multi2,Math.min(multi3,multi5));
             dp[i] = min;
-            if(min==multi2) multi2 = 2*dp[++index2];
-            if(min==multi3) multi3 = 3*dp[++index3];
+            if(min == multi2) multi2 = 2*dp[++index2];
+            if(min == multi3) multi3 = 3*dp[++index3];
             if(min == multi5) multi5 = 5*dp[++index5];
-            
         }
         return dp[n-1];
     }
