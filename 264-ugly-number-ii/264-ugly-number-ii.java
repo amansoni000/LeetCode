6c4129b5fb3a1765c225f19c888a63c879afbc1f
Aman Swarnkar <80588457+amansoni000@users.noreply.github.com>
2022-01-12 13:26:49 +0530

    Time: 2 ms (98.77%), Space: 38.5 MB (50.77%) - LeetHub

---
+++ 264-ugly-number-ii/264-ugly-number-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int nthUglyNumber(int n) {
+        int[] dp = new int[n];
+        int index2=0, index3=0, index5=0;
+        int multi2=2, multi3=3, multi5=5;
+        int min = Integer.MAX_VALUE;
+        dp[0] = 1;
+        for(int i=1; i<n; i++ ){
+            min = Math.min(multi2,Math.min(multi3,multi5));
+            dp[i] = min;
+            if(min==multi2) multi2 = 2*dp[++index2];
+            if(min==multi3) multi3 = 3*dp[++index3];
+            if(min == multi5) multi5 = 5*dp[++index5];
+            
+        }
+        return dp[n-1];
+    }
+}
