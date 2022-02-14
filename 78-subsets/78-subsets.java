b77519451da517e7f2f542bdd98ec14f4738f48d
Aman Swarnkar <80588457+amansoni000@users.noreply.github.com>
2022-02-14 11:38:43 +0530

    Time: 1 ms (78.01%), Space: 43.2 MB (16.22%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -3,10 +3,10 @@ class Solution {
         List<List<Integer>> ls = new ArrayList<>();
         ls.add(new  ArrayList<>());
         for(int i=0; i<nums.length; i++){
-            int len = ls.size();
-            for(int j=0; j<len; j++){
+            for(int j=0; j<ls.size(); j++){
                 List<Integer> x = new ArrayList<>(ls.get(j));
                 x.add(nums[i]);
+                if(x.size() > 1 && x.get(0) == x.get(1)) break;
                 ls.add(x);
             }
         }
