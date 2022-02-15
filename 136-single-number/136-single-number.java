b505157d3afc8d3e8decb5d648b4648c3d02b0ee
Aman Swarnkar <80588457+amansoni000@users.noreply.github.com>
2022-02-15 15:21:18 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (71.93%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,8 +1,8 @@
 class Solution {
     public int singleNumber(int[] nums) {
         // XOR 
-        int result=0, n=nums.length;
-        for(int i=0;i<n;i++){
+        int result = 0, n = nums.length;
+        for(int i=0; i<n; i++){
             result ^= nums[i];
         }
         return result;
