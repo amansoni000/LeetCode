76fda2bb9b423ac002310718395f8a7bf94b4f84
Aman Swarnkar <80588457+amansoni000@users.noreply.github.com>
2022-02-21 11:48:17 +0530

    Time: 1 ms (99.96%), Space: 46 MB (72.72%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,22 +1,17 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        int count = 0, last = nums[0], count2 = 0, mazor = 0;
-        for(int i=0; i<nums.length; i++){
-            if(nums[i] == last) count++;
-            else{
-                if(count > count2){
-                    mazor = last;
-                    count2 = count;
-                }
-                count = 1;
-                last = nums[i];
-            } 
+        int count = 0;
+        int x = 0;
+        for(int i : nums){
+            if(count == 0){
+                x=i;
+            }
+            if(i == x){
+                count++;
+            }else{
+                count--;
+            }
         }
-        if(count > count2){
-            mazor = last;
-            count2 = count;
-        }
-        return mazor;
+        return x;
     }
 }
