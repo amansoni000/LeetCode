ecb74b0131811cbd106bf200fe5f352e9d242143
Aman Swarnkar <80588457+amansoni000@users.noreply.github.com>
2022-03-16 13:29:25 +0530

    Time: 2 ms (89.31%), Space: 45 MB (52.55%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.java
+++ 946-validate-stack-sequences/946-validate-stack-sequences.java
@@ -2,8 +2,8 @@ class Solution {
     public boolean validateStackSequences(int[] pushed, int[] popped) {
         List<Integer> list_push = new ArrayList<>();
         int i = 0, j = 0;
-        while((i < pushed.length && j < pushed.length) || list_push.size() > 0){
-            if( (i < pushed.length && j < pushed.length) && pushed[i] == popped[j]){
+        while(j < pushed.length){
+            if((i < pushed.length && j < pushed.length) && pushed[i] == popped[j]){
                 i++;
                 j++;
             }
