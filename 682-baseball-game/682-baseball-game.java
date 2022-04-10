0b5a987fb7eacd003b7e57040d48ccf026f577ee
Aman Swarnkar <80588457+amansoni000@users.noreply.github.com>
2022-04-10 23:03:32 +0530

    Time: 2 ms (97.05%), Space: 40.1 MB (96.98%) - LeetHub

--- 682-baseball-game/682-baseball-game.java
+++ 682-baseball-game/682-baseball-game.java
@@ -4,19 +4,23 @@ class Solution {
         int sum = 0;
         for(int i = 0; i < ops.length; i++){
             if(ops[i].equals("C")){
+                sum -= list.get(list.size() - 1);
                 list.remove(list.size() - 1);
             }
             else if(ops[i].equals("D")){
+                sum += list.get(list.size() - 1)*2;
                 list.add(list.get(list.size() - 1)*2);
             }
             else if(ops[i].equals("+")){
+                sum += list.get(list.size() - 1) + list.get(list.size() - 2);
                 list.add(list.get(list.size() - 1) + list.get(list.size() - 2));
             }
             else{
+                sum += Integer.parseInt(ops[i]);
                 list.add(Integer.parseInt(ops[i]));
             }
         }
-        for(int i : list) sum += i;
+        // for(int i : list) sum += i;
         return sum;
     }
 }
