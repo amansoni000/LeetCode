0c7dd393ce28f5b23547d757881ef43fdf18477a
Aman Swarnkar <80588457+amansoni000@users.noreply.github.com>
2022-04-16 20:23:07 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (83.27%) - LeetHub

--- 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.java
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.java
@@ -14,23 +14,15 @@
  * }
  */
 class Solution {
-    boolean flag = true;
-    int k = 0, sum = 0;
-    List<Integer> list = new ArrayList<>();
+    int sum = 0;
     public void traversal(TreeNode root){
         if(root == null) return;
-        traversal(root.left);
-        if(flag == true) list.add(root.val);
-        else root.val = list.get(k++);
         traversal(root.right);
+        root.val += sum;
+        sum = root.val;
+        traversal(root.left);
     }
     public TreeNode convertBST(TreeNode root) {
-        traversal(root);
-        flag = false;
-        for(int i = list.size() - 1; i >= 0; i--){
-            sum += list.get(i);
-            list.set(i, sum);
-        }
         traversal(root);
         return root;
     }
