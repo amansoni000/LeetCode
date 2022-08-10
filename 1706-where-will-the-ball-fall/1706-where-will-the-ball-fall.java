ff88eed20e02fd22f8f9e3b3dd75cb94ee02cdc6
Aman Swarnkar <80588457+amansoni000@users.noreply.github.com>
2022-08-10 20:32:37 +0530

    Time: 0 ms (100.00%), Space: 54.9 MB (8.17%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
@@ -17,13 +17,10 @@ class Solution {
                 else break;
             }
         }
-        if( i >= grid.length && j >= 0 && j < grid[0].length){
-            return j;
-        }
+        if( i >= grid.length && j >= 0 && j < grid[0].length) return j;
         else return -1;
     }
     public int[] findBall(int[][] grid) {
-        int row = grid.length;
         int col = grid[0].length;
         int[] arr = new int[col];
         for(int i = 0; i < col; i++){
