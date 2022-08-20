1c29ebe85648d1599748e5357dcc6e9fd29bd253
Aman Swarnkar <80588457+amansoni000@users.noreply.github.com>
2022-08-20 19:46:12 +0530

    Time: 1 ms (90.78%), Space: 48.8 MB (6.73%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.java
@@ -0,0 +1,22 @@
+class Solution {
+    int count = 0;
+    public void helper(int[][] image, int i, int j, int c, int x){
+        if(i >= image.length || j >= image[0].length || i < 0 || j < 0 || image[i][j] == c || image[i][j] != x){
+            return;
+        }
+        if(image[i][j] == x){
+            image[i][j] = c;
+            // count += 1;
+            // return;
+        }
+        helper(image, i + 1, j, c, x);
+        helper(image, i - 1, j, c, x);
+        helper(image, i , j + 1, c, x);
+        helper(image, i , j - 1, c, x);        
+    }
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        helper(image, sr, sc, color, image[sr][sc]);
+            // return count;
+        return image;
+    }
+}
