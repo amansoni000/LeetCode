04ce5a8ee389ebf79c35bb969ef8d142af000338
Aman Swarnkar <80588457+amansoni000@users.noreply.github.com>
2022-08-25 13:14:25 +0530

    Time: 2 ms (99.48%), Space: 42.7 MB (91.34%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] arr1 = new int[26];
+        int[] arr2 = new int[26];
+        for(char c : magazine.toCharArray()){
+            arr1[c-'a']++;
+        }
+        for(char c : ransomNote.toCharArray()){
+            arr2[c-'a']++;
+        }
+        for(int i = 0; i < 26; i++){
+            if(arr1[i] < arr2[i]) return false;
+        }
+        return true;
+    }
+}
