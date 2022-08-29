c224767c4c556bec8b96220514dce9cd608ff4da
Aman Swarnkar <80588457+amansoni000@users.noreply.github.com>
2022-08-29 20:33:45 +0530

    Time: 4 ms (76.67%), Space: 57.6 MB (44.62%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void traversal(char[][] grid, int i, int j){
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0'){
+            return;
+        }
+        grid[i][j] = '0';
+        traversal(grid, i + 1, j);
+        traversal(grid, i - 1, j);
+        traversal(grid, i, j + 1);
+        traversal(grid, i, j - 1);
+    }
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == '1'){
+                    count++;
+                    traversal(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+}
