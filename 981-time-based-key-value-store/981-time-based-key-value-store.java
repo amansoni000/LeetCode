c4bdcb8c8a638c058531b523401337bfec109635
Aman Swarnkar <80588457+amansoni000@users.noreply.github.com>
2022-10-06 13:15:52 +0530

    Time: 399 ms (31.69%), Space: 219.7 MB (34.71%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.java
+++ 981-time-based-key-value-store/981-time-based-key-value-store.java
@@ -1,39 +1,25 @@
 class TimeMap {
-    HashMap<String, HashMap<Integer, String>> ValuesMap;
-    HashMap<String, List<Integer>> timeMap;
+    HashMap<String, TreeMap<Integer, String>> ValuesMap;
     public TimeMap() {
         ValuesMap = new HashMap<>();
-        timeMap = new HashMap<>();
     }
     
     public void set(String key, String value, int timestamp) {
-        HashMap<Integer, String> tempMap = ValuesMap.getOrDefault(key, new HashMap<>());
-        tempMap.put(timestamp, value);
-        ValuesMap.put(key, tempMap);
-        
-        List<Integer> time_list = timeMap.getOrDefault(key, new ArrayList<>());
-        time_list.add(timestamp);
-        timeMap.put(key, time_list);
+        TreeMap<Integer, String> temp = ValuesMap.getOrDefault(key, new TreeMap<>());
+        temp.put(timestamp, value);
+        ValuesMap.put(key, temp);
     }
     
     public String get(String key, int timestamp) {
-        if(ValuesMap.containsKey(key)){
-            if(ValuesMap.get(key).containsKey(timestamp)) return ValuesMap.get(key).get(timestamp);
-            List<Integer> list = timeMap.get(key);
-            if(timestamp < list.get(0)) return "";
-            int l = 0, r = list.size()-1, ans = 0;
-            while(l <= r){
-                int mid = (l+r)/2;
-                if(list.get(mid) < timestamp){
-                    ans = mid;
-                    l = mid+1;
-                }
-                else if(list.get(mid) > timestamp) r = mid - 1;
-                }
-                // System.out.println(map.get(key));
-            return ValuesMap.get(key).get(list.get(ans));
+        TreeMap<Integer, String> tree = ValuesMap.get(key);
+        if (tree == null){
+            return "";
         }
-        else return "";
+        Integer floor = tree.floorKey(timestamp);
+        if ( floor== null){
+            return "";
+        }
+        return tree.get(floor);
     }
 }
 
