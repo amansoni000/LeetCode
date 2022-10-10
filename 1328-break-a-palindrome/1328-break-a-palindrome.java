fc998bf1ae67de81d0b2f601953ee3db860ca825
Aman Swarnkar <80588457+amansoni000@users.noreply.github.com>
2022-10-10 08:17:41 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (66.45%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.java
+++ 1328-break-a-palindrome/1328-break-a-palindrome.java
@@ -2,24 +2,14 @@ class Solution {
     public String breakPalindrome(String palindrome) {
         if(palindrome.length() == 1) return "";
         char[] arr = palindrome.toCharArray();
-        boolean flag = true;
-        if(arr[0] != 'a') arr[0] = 'a';
-        else{
-            for(int i = 1; i < palindrome.length(); i++){
-                if(arr[i] > arr[i-1]){
-                    if(arr.length%2 != 0 && i == arr.length/2) continue;
-                    // System.out.println(i);
-                    if(arr[i] == 'a') arr[i] = 'b';
-                    else arr[i] = 'a';
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag){
-                if(arr[arr.length - 1] == 'a') arr[arr.length - 1] = 'b';
-                else arr[arr.length - 1] = 'a';
+        for(int i = 0; i < palindrome.length()/2; i++){
+            if(arr[i] != 'a'){
+                arr[i] = 'a';
+                return String.valueOf(arr);
             }
         }
+        if(arr[arr.length - 1] == 'a') arr[arr.length - 1] = 'b';
+        else arr[arr.length - 1] = 'a';
         return String.valueOf(arr);
     }
 }
