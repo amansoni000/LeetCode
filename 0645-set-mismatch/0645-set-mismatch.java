95d8b512ebd1f59f243d5a3cad1002c77e673f8d
Aman Swarnkar <80588457+amansoni000@users.noreply.github.com>
2022-10-23 12:06:21 +0530

    Time: 13 ms (33.57%), Space: 54.5 MB (57.65%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,11 +1,11 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
         int sum = 0, og_sum = 0, n = nums.length, a = -1;
+        int[] freq = new int[n+1];
         for(int x : nums){
             sum += x;
-            if(set.contains(x)) a = x;
-            set.add(x);
+            if(freq[x] > 0) a = x;
+            freq[x]++;
         }
         og_sum = (n*(n+1))/2;
         int b = og_sum - sum + a;
