f6c94fb6114ed1585f7a60d071523856f8183a3a
Aman Swarnkar <80588457+amansoni000@users.noreply.github.com>
2022-12-04 12:21:05 +0530

    Time: 16 ms (99.13%), Space: 60.2 MB (94.98%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.java
+++ 2256-minimum-average-difference/2256-minimum-average-difference.java
@@ -1,20 +1,18 @@
 class Solution {
     public int minimumAverageDifference(int[] nums) {
-        long[] left = new long[nums.length];
         long[] right = new long[nums.length];
         long sum = 0;
-        for(int i = 0; i < nums.length; i++){
-            sum += (long)nums[i];
-            left[i] = sum;
-        }
-        sum = 0;
+
         for(int i = nums.length - 1; i >= 0; i--){
             right[i] = sum;
             sum += (long)nums[i];
         }
+        
         long min = Integer.MAX_VALUE, ans = 0;
+        sum = 0;
         for(int i = 0; i < nums.length-1; i++){
-            long l = left[i]/(i+1);
+            sum += (long)nums[i];
+            long l = sum/(i+1);
             long r = right[i]/(nums.length-i-1);
             long abs = Math.abs(l - r);
             if(abs < min){
@@ -22,7 +20,7 @@ class Solution {
                 min = abs;
             }
         }
-        long abs = Math.abs(left[nums.length-1]/nums.length);
+        long abs = Math.abs((sum + (long)nums[nums.length-1])/nums.length);
         if(abs < min){
             ans = nums.length-1;
         }
