0487e68bd3db00172a07c5f0e146ff779f7e5b2a
Aman Swarnkar <80588457+amansoni000@users.noreply.github.com>
2023-01-02 18:21:07 +0530

    Time: 1 ms (99.98%), Space: 44.3 MB (60.23%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -0,0 +1,29 @@
+class Solution {
+    int n;
+    int[][] newGraph;
+    List<List<Integer>> result = new ArrayList<>();
+    public void helper(int[] arr, List<Integer> temp){
+        for(int i = 0; i < arr.length; i++){
+            temp.add(arr[i]);
+            if(arr[i] == n){
+                // System.out.println(temp);
+                List<Integer> dummy = new ArrayList<>(temp);
+                result.add(dummy);
+                // return;
+            }
+            else{
+                helper(newGraph[arr[i]], temp);
+            }
+            temp.remove(temp.size() - 1);
+        }
+    }
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        n = graph.length - 1;
+        newGraph = graph;
+        
+        List<Integer> temp = new ArrayList<>();
+        temp.add(0);
+        helper(graph[0], temp);
+        return result;
+    }
+}
